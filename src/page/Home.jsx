976bc44b9047663671d8a8201c582dd8d1820dfa
Tristandac28 <tdacier@siclade.fr>
2023-10-13 17:00:55 +0200

    correction home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -33,9 +33,18 @@ function Home() {
   };
 
   return (
-    <div>
-      <div className="relative">
-        <img src={images[currentImageIndex].src} alt={images[currentImageIndex].title} />
+    <div className="flex flex-col items-center">
+      <div className="relative w-full h-screen overflow-hidden">
+        {images.map((image, index) => (
+          <img
+            key={image.id}
+            src={image.src}
+            alt={image.title}
+            className={`absolute top-0 left-0 w-full h-full transition-opacity duration-500 ${
+              index === currentImageIndex ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+        ))}
         <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center text-white">
           <h1 className="text-4xl font-bold">{images[currentImageIndex].title}</h1>
           <p className="text-lg">{images[currentImageIndex].description}</p>
@@ -52,6 +61,30 @@ function Home() {
         >
           &gt;
         </button>
+      </div>
+
+      <div className="max-w-7xl mx-auto mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="https://picsum.photos/id/1018/400/400" alt="Image 1" className="w-full h-48 object-cover" />
+          <div className="p-4">
+            <h2 className="text-lg font-bold mb-2">Image 1</h2>
+            <p className="text-gray-700">Description de l'image 1</p>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="https://picsum.photos/id/1015/400/400" alt="Image 2" className="w-full h-48 object-cover" />
+          <div className="p-4">
+            <h2 className="text-lg font-bold mb-2">Image 2</h2>
+            <p className="text-gray-700">Description de l'image 2</p>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          <img src="https://picsum.photos/id/1019/400/400" alt="Image 3" className="w-full h-48 object-cover" />
+          <div className="p-4">
+            <h2 className="text-lg font-bold mb-2">Image 3</h2>
+            <p className="text-gray-700">Description de l'image 3</p>
+          </div>
+        </div>
       </div>
     </div>
   );
